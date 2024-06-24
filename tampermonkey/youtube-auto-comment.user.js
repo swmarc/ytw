b7cc8cf0ed9648bc86410775c40cbf3aecc4efd3
Marcel Saegebarth <marc@mos6581.de>
2024-06-24 19:51:29 +0200

    Fix missing new version number

--- tampermonkey/youtube-auto-comment.user.js
+++ tampermonkey/youtube-auto-comment.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Auto Comment After Watch Time
 // @namespace    http://tampermonkey.net/
-// @version      2.0.0
+// @version      2.0.2
 // @description  Automatically comments a YouTube video after watching a specified percentage
 // @author       swmarc
 // @downloadURL  https://github.com/swmarc/ytw/raw/main/tampermonkey/youtube-auto-comment.user.js
