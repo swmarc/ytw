8d6eaadc03aeb837869de2d5aceaf7be1ccb297a
Marcel Saegebarth <marc@mos6581.de>
2024-06-25 16:47:22 +0200

    Pretty print `sleep` & `discord` hook libs

--- lib/hooks/discord.sh
+++ lib/hooks/discord.sh
@@ -2,6 +2,13 @@
 
 set -eu
 
+YTW_LIB_HOOKS_DISCORD="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+# shellcheck source=../datetime.sh
+source "${YTW_LIB_HOOKS_DISCORD}/../datetime.sh"
+# shellcheck source=../print.sh
+source "${YTW_LIB_HOOKS_DISCORD}/../print.sh"
+
 ytw.lib.hooks.discord.hook() {
     local TITLE="${1}"
     local BODY="${2}"
@@ -10,44 +17,45 @@ ytw.lib.hooks.discord.hook() {
     local YOUTUBE_IMAGE_LINK="https://i.ytimg.com/vi"
     local YOUTUBE_IMAGE_FILE="hqdefault.jpg"
     local DATETIME=""
-    
+
     if [ -z "${DISCORD_WEBHOOK}" ]; then
         return
     fi
-    
+
     while true; do
-        DATETIME=$(echo "[$(date -u --rfc-3339=seconds)]")
-        echo -n "[+++] ${DATETIME} Calling Discord hook."
-        
+        DATETIME=$(ytw.lib.datetime.get)
+        echo -ne "$(ytw.lib.print.bold "[$(ytw.lib.print.green "+++")]") "
+        echo -ne "$(ytw.lib.print.bold "[${DATETIME}]") "
+        echo -n "Calling Discord hook."
+
         curl -0 -S -s -o /dev/null -X POST "${DISCORD_WEBHOOK}" \
-        -H "Expect:" \
-        -H 'Content-Type: application/json; charset=utf-8' \
-        -d \
-        "{
-        \"content\": null,
-        \"embeds\": [
-          {
-            \"title\": \"${TITLE}\",
-            \"description\": \"${BODY}\",
-            \"url\": \"${YOUTUBE_VIDEO_LINK}${YOUTUBE_VIDEO_ID}\",
-            \"image\": {
-                \"url\": \"${YOUTUBE_IMAGE_LINK}/${YOUTUBE_VIDEO_ID}/${YOUTUBE_IMAGE_FILE}\"
-            },
-            \"color\": 2752256,
-            \"author\": {
-              \"name\": \"YTW\",
-              \"icon_url\": \"https://cdn-icons-png.flaticon.com/512/3670/3670209.png\"
-            }
-          }
-        ]
-        }" && echo " Done." && break
-        
-        DATETIME=$(echo "[$(date -u --rfc-3339=seconds)]")
-        printf "[!!!] %s Calling Discord hook failed.\n" ${DATETIME}
-        
-        DATETIME=$(echo "[$(date -u --rfc-3339=seconds)]")
-        printf "[!!!] %s Retrying in 1 minute.\n" ${DATETIME}
-        
+            -H "Expect:" \
+            -H 'Content-Type: application/json; charset=utf-8' \
+            -d \
+            "{
+                \"content\": null,
+                \"embeds\": [
+                  {
+                    \"title\": \"${TITLE}\",
+                    \"description\": \"${BODY}\",
+                    \"url\": \"${YOUTUBE_VIDEO_LINK}${YOUTUBE_VIDEO_ID}\",
+                    \"image\": {
+                        \"url\": \"${YOUTUBE_IMAGE_LINK}/${YOUTUBE_VIDEO_ID}/${YOUTUBE_IMAGE_FILE}\"
+                    },
+                    \"color\": 2752256,
+                    \"author\": {
+                      \"name\": \"YTW\",
+                      \"icon_url\": \"https://cdn-icons-png.flaticon.com/512/3670/3670209.png\"
+                    }
+                  }
+                ]
+            }" && echo " Done." && break
+
+        DATETIME=$(ytw.lib.datetime.get)
+        echo -ne "$(ytw.lib.print.bold "[$(ytw.lib.print.red "!!!")]") "
+        echo -ne "$(ytw.lib.print.bold "[${DATETIME}]") "
+        echo "Calling Discord hook failed. Retrying in 1 minute."
+
         sleep 60
     done
 }
--- lib/sleep.sh
+++ lib/sleep.sh
@@ -3,40 +3,58 @@
 set -eu
 
 YTW_LIB_SLEEP="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+# shellcheck source=datetime.sh
+source "${YTW_LIB_SLEEP}/datetime.sh"
+# shellcheck source=print.sh
 source "${YTW_LIB_SLEEP}/print.sh"
 
-ytw.lib.sleep.seconds () {
-  local SECONDS=$1
-  local PREFIX="s"
+ytw.lib.sleep.seconds() {
+    local SECONDS=$1
+    local PREFIX="s"
 
-  for ((i = SECONDS; i > 0; i--)); do
-    if [ $i -eq 1 ]; then
-      PREFIX=""
-    fi
+    for ((i = SECONDS; i > 0; i--)); do
+        if [ $i -eq 1 ]; then
+            PREFIX=""
+        fi
 
-    echo -ne "\033[1K\r\e[1;33m***\e[0m Sleeping for "
-    ytw.lib.print.green "${i}"
-    echo -ne " second${PREFIX}."
-    sleep 1
-  done
+        DATETIME=$(ytw.lib.datetime.get)
+        echo -ne "\033[1K\r"
+        echo -ne "$(ytw.lib.print.bold "[$(ytw.lib.print.yellow "***")]") "
+        echo -ne "$(ytw.lib.print.bold "[${DATETIME}]") "
+        echo -n "Sleeping for "
+        echo -ne "$(ytw.lib.print.blue_light "${i}") "
+        echo -n "second${PREFIX}."
 
-  echo -ne "\033[1K\r\e[1;33m***\e[0m Sleeping for "
-  ytw.lib.print.green "0"
-  echo " seconds."
+        sleep 1
+    done
+
+    DATETIME=$(ytw.lib.datetime.get)
+    echo -ne "\033[1K\r"
+    echo -ne "$(ytw.lib.print.bold "[$(ytw.lib.print.yellow "***")]") "
+    echo -ne "$(ytw.lib.print.bold "[${DATETIME}]") "
+    echo -n "Sleeping for "
+    echo -ne "$(ytw.lib.print.blue_light 0) "
+    echo "seconds."
 }
 
-ytw.lib.sleep.minutes () {
-  local MINUTES=$1
+ytw.lib.sleep.minutes() {
+    local MINUTES=$1
 
-  for ((i = MINUTES; i > 0; i--)); do
-    if [ $i -eq 1 ]; then
-      ytw.lib.sleep.seconds 60
-      continue
-    fi
+    for ((i = MINUTES; i > 0; i--)); do
+        if [ $i -eq 1 ]; then
+            ytw.lib.sleep.seconds 60
+            continue
+        fi
 
-    echo -ne "\033[1K\r\e[1;33m***\e[0m Sleeping for "
-    ytw.lib.print.green "${i}"
-    echo -ne " minutes."
-    sleep 60
-  done
+        DATETIME=$(ytw.lib.datetime.get)
+        echo -ne "\033[1K\r"
+        echo -ne "$(ytw.lib.print.bold "[$(ytw.lib.print.yellow "***")]") "
+        echo -ne "$(ytw.lib.print.bold "[${DATETIME}]") "
+        echo -n "Sleeping for "
+        echo -ne "$(ytw.lib.print.blue_light "${i}") "
+        echo -n "minutes."
+
+        sleep 60
+    done
 }
