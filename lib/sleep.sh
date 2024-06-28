2f51a0cb403168506518e8cde8f524e2ea9788b7
Marcel Saegebarth <marc@mos6581.de>
2024-06-28 19:37:42 +0200

    Render every second of a minute
    In case the actual progress got overwritten by another message.

--- lib/sleep.sh
+++ lib/sleep.sh
@@ -42,20 +42,24 @@ ytw.lib.sleep.minutes() {
     local -ir MINUTES=$1
     local -r PREFIX=${2:-""}
     local DATETIME=""
+    local -i SECONDS=60
 
-    for ((i = MINUTES; i > 0; i--)); do
-        if [ $i -eq 1 ]; then
+    for ((MINUTE = MINUTES; MINUTE > 0; MINUTE--)); do
+        if [ $MINUTE -eq 1 ]; then
             ytw.lib.sleep.seconds 60 "${PREFIX}"
             continue
         fi
 
-        DATETIME=$(ytw.lib.datetime.get)
-        echo -ne \
-            "\033[1K\r$(ytw.lib.print.bold "[$(ytw.lib.print.yellow "***")]")" \
-            "$(ytw.lib.print.bold "[${DATETIME}]")" \
-            "${PREFIX}" \
-            "$(ytw.lib.print.yellow "${i}") minutes remaining."
+        # Render out any second again in case it got overwritten for too long due to other messages.
+        for ((SECOND = SECONDS; SECOND > 0; SECOND--)); do
+            DATETIME=$(ytw.lib.datetime.get)
+            echo -ne \
+                "\033[1K\r$(ytw.lib.print.bold "[$(ytw.lib.print.yellow "***")]")" \
+                "$(ytw.lib.print.bold "[${DATETIME}]")" \
+                "${PREFIX}" \
+                "$(ytw.lib.print.yellow "${MINUTE}") minutes remaining."
 
-        sleep 60
+            sleep 1
+        done
     done
 }
